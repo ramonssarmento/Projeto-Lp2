6559f7d39b842c36bdf75ef392cf48dfe8c155ec
herculesra <hercules.anselmo@ccc.ufcg.edu.br>
2018-07-12 11:52:36 -0300

    classe teste para itens por quilo

--- src/test/ItemQuiloTest.java
+++ src/test/ItemQuiloTest.java
@@ -20,5 +20,21 @@ public class ItemQuiloTest {
 	public void testToString() {
 		assertEquals("1. Peito de frango, alimentos nao industrializados, Preco por quilo: <O Frangao, R$ 6,00;>", item.toString());
 	}
+	
+	@Test
+	public void testEqualsItensIguais() {
+		ItemQuilo item1 = new ItemQuilo(1, "pernil", "alimentos nao industrializados", 0.75, "O Frangao", 6.78);
+		ItemQuilo item2 = new ItemQuilo(1, "pernil", "alimentos nao industrializados", 1, "Braza", 8.00);
+		assertEquals(true, item1.equals(item2));
+	}
+	
+	@Test
+	public void testEqualsItensDiferentes() {
+		ItemQuilo item1 = new ItemQuilo(1, "perna de boi", "alimentos nao industrializados", 1, "O Frangao", 8.00);
+		ItemQuilo item2 = new ItemQuilo(1, "pernil", "alimentos nao industrializados", 1, "Braza", 8.00);
+		assertEquals(false, item1.equals(item2));
+	}
+	
+
 
 }
