551e34f93dec07e0ebc19821f44a920d400624e8
herculesra <hercules.anselmo@ccc.ufcg.edu.br>
2018-07-13 13:10:41 -0300

    Classe teste de ItemQuilo pronta

--- src/test/ItemQuiloTest.java
+++ src/test/ItemQuiloTest.java
@@ -35,6 +35,103 @@ public class ItemQuiloTest {
 		assertEquals(false, item1.equals(item2));
 	}
 	
-
-
+	
+	// Testes para construtor
+	@Test(expected = RuntimeException.class)
+	public void testNomeVazio() {
+		item = new ItemQuilo(1, "", "alimento nao industrializado", 0.6, "O Frangao", 6.00);
+	}
+	
+	@Test(expected = RuntimeException.class)
+	public void testNomeNull() {
+		item = new ItemQuilo(1, null, "alimento nao industrializado", 0.6, "O Frangao", 6.00);
+	}
+	
+	@Test(expected = RuntimeException.class)
+	public void testCategoriaVazia() {
+		item = new ItemQuilo(1, "Peito de frango", "", 0.6, "O Frangao", 6.00);
+	}
+	
+	@Test(expected = RuntimeException.class)
+	public void testCategoriaNull() {
+		item = new ItemQuilo(1, "Peito de frango", null, 0.6, "O Frangao", 6.00);
+	}
+	
+	@Test(expected = RuntimeException.class)
+	public void testCategoriaInvalida() {
+		item = new ItemQuilo(1, "Peito de frango", "Carnes", 0.6, "O Frangao", 6.00);
+	}
+	
+	@Test(expected = RuntimeException.class)
+	public void testKiloNegativo() {
+		item = new ItemQuilo(1, "Peito de frango", "alimento nao industrializado", -1, "O Frangao", 6.00);
+	}
+	
+	@Test(expected = RuntimeException.class)
+	public void testSuperMercadoVazio() {
+		item = new ItemQuilo(1, "Peito de frango", "alimento nao industrializado", 0.6, "", 6.00);
+	}
+	
+	@Test(expected = RuntimeException.class)
+	public void testSuperMercadoNull() {
+		item = new ItemQuilo(1, "Peito de frango", "alimento nao industrializado", 0.6, null, 6.00);
+	}
+	
+	@Test(expected = RuntimeException.class)
+	public void testPrecoNegativo() {
+		item = new ItemQuilo(1, "Peito de frango", "alimento nao industrializado", 0.6, "O Frangao", -2.00);
+	}
+	
+	// Testes para metodo Atualiza
+	@Test(expected = RuntimeException.class)
+	public void testAtualizaAtributoVazio() {
+		item.atualizaItem("", "peixe");
+	}
+	
+	@Test(expected = RuntimeException.class)
+	public void testAtualizaAtributoNull() {
+		item.atualizaItem(null, "peixe");
+	}
+	
+	@Test(expected = RuntimeException.class)
+	public void testAtualizaNovoValorVazio() {
+		item.atualizaItem("nome", "");
+	}
+	
+	@Test(expected = RuntimeException.class)
+	public void testAtualizaNovoValorNull() {
+		item.atualizaItem("nome", null);
+	}
+	
+	@Test(expected = RuntimeException.class)
+	public void testAtualizaCategoriaValorInvalido() {
+		item.atualizaItem("categoria", "xula");
+	}
+	
+	@Test(expected = RuntimeException.class)
+	public void testAtualizaQuiloNegativo() {
+		item.atualizaItem("kg", "-0.6");
+	}
+	
+	@Test(expected = RuntimeException.class)
+	public void testAtualizaQuiloInvalido() {
+		item.atualizaItem("kg", "Dois");
+	}
+	// Testes para AdicionaPrecoItem
+	
+	@Test(expected = RuntimeException.class)
+	public void testadicionaPrecoItemLocalDeCompraVazio() {
+		item.adicionaPrecoItem("", 6);
+	}
+	
+	@Test(expected = RuntimeException.class)
+	public void testadicionaPrecoItemLocalDeCompraNull() {
+		item.adicionaPrecoItem(null, 6);
+	}
+	
+	@Test(expected = RuntimeException.class)
+	public void testadicionaPrecoItemNegativo() {
+		item.adicionaPrecoItem("Mercadao", -6);
+	}
+	
 }
