b31f430b908a52eded03a8fcf54e3a5aab9e9a0c
Ramon Sousa Sarmento <ramon.sarmento@ccc.ufcg.edu.br>
2018-07-17 09:12:49 -0300

    -Rhistory

--- src/classes/ProdutoLista.java
+++ src/classes/ProdutoLista.java
@@ -1,38 +1,38 @@
 package classes;
 
 public class ProdutoLista {
-	
+
 	private int id;
 	private Item produto;
 	private int quantidade;
-	
-	
+
 	public ProdutoLista(Item produto, int quantidade) {
-		
+
 		this.produto = produto;
 		this.id = produto.getId();
 		this.quantidade = quantidade;
-		
+
 	}
-	
+
 	public void setQuantidade(int novaQuantidade) {
-		
+
 		this.quantidade = novaQuantidade;
 	}
-	
+
 	public int getId() {
-		
+
 		return this.id;
-	
+
 	}
-	
+
 	public int getQuantidade() {
 		return this.quantidade;
 	}
-	
-	public toString() {
-	
-		return String.format("%i %s, %s, %s", (this.quantidade, this.produto.getNome(), this.produto.getCategoria(), this.produto.getQuantidade()));
+
+	public String toString() {
+
+		return String.format("%d %s, %s, %s", this.quantidade, this.produto.getNome(), this.produto.getCategoria(),
+				this.produto.getQuantidade());
 	}
-	
+
 }
